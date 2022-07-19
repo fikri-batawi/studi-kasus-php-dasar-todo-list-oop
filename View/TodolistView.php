7303de8729e5200bf8d12d70dda83b8914d78ded
Muhammad Fikri <fikri2347@gmail.com>
2022-07-19 19:21:00 +0700

    Membuat view

---
+++ View/TodolistView.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace View {
+
+  class TodolistView {
+
+    public function showTodolist(): void
+    {
+      
+    }
+    public function addTodolist(): void
+    {
+
+    }
+    public function removeTodolist(): void
+    {
+
+    }
+
+  }
+
+}
