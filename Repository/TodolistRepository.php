70e3fa03a677456520c65589030ad487bd3b857b
Muhammad Fikri <fikri2347@gmail.com>
2022-07-19 19:28:20 +0700

    Repository dan service : Menampilkan todolist

--- Repository/TodolistRepository.php
+++ Repository/TodolistRepository.php
@@ -4,13 +4,34 @@ namespace Repository {
 
     use Entity\Todolist;
 
-  interface Repository {
+  interface TodolistRepository {
 
     public function save(Todolist $todolist): void;
 
     public function remove(int $number): bool;
 
     public function findAll(): array;
+
+  }
+
+  class TodolistRepositoryImpl implements TodolistRepository {
+
+    private array $todolist = array();
+
+    function save(Todolist $todolist): void
+    {
+      
+    }
+
+    function remove(int $number): bool
+    {
+      return false;  
+    }
+
+    function findAll(): array
+    {
+      return $this->todolist;
+    }
 
   }
 
--- Service/TodolistService.php
+++ Service/TodolistService.php
@@ -2,6 +2,8 @@
 
 namespace Service {
 
+  use Repository\TodolistRepositoryImpl;
+
   interface TodolistService {
 
     public function addTodolist(string $string): void;
@@ -9,6 +11,31 @@ namespace Service {
     public function removeTodolist(int $number): void;
 
     public function showTodolist(): void;
+
+  }
+
+  class TodolistServiceImpl implements TodolistService {
+
+    private TodolistRepositoryImpl $todolistRepository;
+
+    function addTodolist(string $string): void
+    {
+      
+    }
+
+    function removeTodolist(int $number): void
+    {
+      
+    }
+
+    function showTodolist(): void
+    {
+      echo "TODOLIST" . PHP_EOL;
+      $todolist = $this->todolistRepository->findAll();
+      foreach($todolist as $number => $value){
+        echo "$number. $value" . PHP_EOL;
+      }
+    }
 
   }
 
